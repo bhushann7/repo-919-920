8ff36bac1fb600d0eb298a91b7470edb8096f7d2
Bhushan Bamble <bhushan.bamble@gmail.com>
2021-03-22 16:41:58 +0530

    Added Add function

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,4 +3,7 @@ class HelloWorld{
 public static void main(String []args){
   System.out.println("Hello Github!");
   }
+  public static void add(int num1,int num2){
+    System.out.println(num1+num2);
+  }
 }
